ea8798ed5f91e5766cf0e3c03bf8fbd627c71380
Zhihao <lzh727@126.com>
2015-03-23 20:12:51 +0800

    Add @baixing.com/net filter

--- config/passport.js
+++ config/passport.js
@@ -1,3 +1,5 @@
+var _ = require('lodash');
+
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var config = require('./config.js');
 
@@ -14,10 +16,13 @@ module.exports = function(passport) {
         clientID: config.oauth2.google.clientID,
         clientSecret : config.oauth2.google.clientSecret,
         callbackURL : config.oauth2.google.callbackURL
-    },
-    function(token, refreshToken, profile, done) {
-        process.nextTick(function() {
-            return done(null, profile);
-        });
-    }));
+    }, validateBaixingEmail));
+};
+
+function validateBaixingEmail(token, refreshToken, profile, done) {
+    var valid = true;
+    profile.emails && _.forEach(profile.emails, function(n) {
+        valid = valid && n.value && /@baixing\.(com|net)$/.test(n.value);
+    });
+    return done(null, valid ? profile : false);
 }
