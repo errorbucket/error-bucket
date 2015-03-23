34aaac9354cf4a2688e6265ad4fa8b164bea9446
Zhihao <lzh727@126.com>
2015-03-23 16:16:06 +0800

    Added passport config

---
+++ config/passport.js
@@ -0,0 +1,24 @@
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config.js');
+
+module.exports = function(passport) {
+    passport.serializeUser(function(user, done) {
+        done(null, user);
+    });
+
+    passport.deserializeUser(function(obj, done) {
+        done(null, obj);
+    });
+
+    passport.use(new GoogleStrategy({
+        clientID: config.oauth2.google.clientID,
+        clientSecret : config.oauth2.google.clientSecret,
+        callbackURL : config.oauth2.google.callbackURL
+    },
+    function(token, refreshToken, profile, done) {
+        console.log(token);
+        process.nextTick(function() {
+            return done(null, profile);
+        });
+    }));
+}
